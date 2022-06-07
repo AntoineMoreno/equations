c8530a7f04fc725f602ed28a8779fe0aada16fa8
Hanna Chartier <hanna.chartier@gmail.com>
2022-06-07 15:56:35 +0200

    camarcheok

--- api/fast.py
+++ api/fast.py
@@ -2,11 +2,16 @@ from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 import numpy as np
 from PIL import Image
+import matplotlib.pyplot as plt
 
 import cv2
 from tensorflow.keras import models
 
-#from equations.train import make_prediction
+from equations.train import make_prediction
+from equations.test_data import test_data
+from equations.utils import to_latex, give_classes
+
+
 
 import joblib
 
@@ -29,15 +34,19 @@ def index():
 
 @app.post("/equations")
 def get_image(file: UploadFile = File(...)):
-    print(type(file.file))
+    #print(type(file.file))
     image = np.array(Image.open(file.file))
 
+    list_images=test_data(image)
+
+    class_names = give_classes()
+
     loaded_model = models.load_model("modelDL")
-    image_3 = np.expand_dims(image, -1)
 
-    resized_3  =  cv2.cvtColor(image_3,cv2.COLOR_GRAY2RGB)
+    a = []
+    for image in list_images:
+        a.append(to_latex(make_prediction(loaded_model, image, class_names)))
+    make_prediction(loaded_model, list_images[0], class_names)
+    b=''.join(a)
 
-    prediction_scores = loaded_model.predict(np.expand_dims(resized_3, axis=0))
-    predicted_index = np.argmax(prediction_scores)
-
-    return {'result' : int(predicted_index)}
+    return {'code LaTeX' : b}
--- equations/utils.py
+++ equations/utils.py
@@ -4,7 +4,7 @@ def to_latex(str):
     """
     Convert regular text to LaTeX code
     """
-    dct={'Delta' : '\Delta', 'alpha' : '\alpha', 'ascii_124' : '|', 'beta':'\beta', 'cos':'\cos', \
+    dct={'Delta' : '\Delta', 'alpha' : '\\alpha', 'ascii_124' : '|', 'beta':'\beta', 'cos':'\cos', \
     'exists':'\exists','forall':'\forall', 'forward_slash':'/', 'gamma':'\gamma', 'geq' :'\geq',\
     'gt':'>', 'in':'\in','infty':'\infty','lambda':'\lambda', 'ldots':'\lodts', 'leq':'\leq',\
     'lim' : '\lim', 'log':'\log','It':'<','mu':'\mu', 'neq':'\neq', 'phi':'\phi','pi':'\pi',\
@@ -16,3 +16,18 @@ def to_latex(str):
 
 def get_class_name(train_ds):
     return tuple(train_ds.class_names)
+
+
+def give_classes():
+    class_names = ['!', '(', ')', '+', ',', '-', '0', '1',
+ '2', '3', '4', '5', '6', '7', '8', '9', '=',
+ 'A', 'C', 'Delta', 'G', 'H', 'M', 'N', 'R', 'S',
+ 'T', 'x', '[', ']', 'alpha', 'ascii_124', 'b',
+ 'beta', 'cos', 'd', 'div', 'e', 'exists', 'f',
+ 'forall', 'forward_slash', 'gamma', 'geq', 'gt',
+ 'i', 'in', 'infty', 'int', 'j', 'k', 'l', 'lambda',
+ 'ldots', 'leq', 'lim', 'log', 'lt', 'mu', 'neq',
+ 'o', 'p', 'phi', 'pi', 'pm', 'prime', 'q',
+ 'rightarrow', 'sigma', 'sin', 'sqrt', 'sum', 'tan',
+ 'theta','times', 'u', 'v','w','y','z','{','}']
+    return class_names
